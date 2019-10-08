d5d6cf17507d0131b6387437aa5bcde8c46300c4
Treehouse <treehouse@Treehouses-MacBook-Air.local>
2019-10-08 10:58:17 -0400

    Completed all routes for courses, except for the 'put' method which is currently suffering from a bug

--- routes/courses.js
+++ routes/courses.js
@@ -5,6 +5,9 @@ const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 
+// Imports the authenticateUser validation function from the validations folder.
+const authenticateUser = require('../validations/authenticate-user.js');
+
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
@@ -46,5 +49,51 @@ router.get('/courses/:id', async (req, res, next) => {
        .status(200).end();
   }
 });
+
+router.post('/courses', authenticateUser, async (req, res, next) => {
+   try {
+     const course = await Course.create(req.body);
+     res.location(`/api/courses/${course.id}`);
+     res.status(201).end();
+   } catch(err) {
+     if (err.name === "SequelizeValidationError" || "SequelizeUniqueConstraintError") {
+       res.status(400).json({error: err.message});
+     } else {
+       return next(err);
+     }
+   }
+});
+
+router.put('/courses/:id', authenticateUser, async (req, res, next) => {
+  try {
+    const course = await Course.findByPk(req.params.id)
+    if (course === null) {
+      res.status(404).json({message: "This course does not exist"});
+    } else {
+      await course.update(req.body);
+      res.status(204).end();
+    }
+  } catch(err) {
+    if (err.name === "SequelizeValidationError" || "SequelizeUniqueConstraintError") {
+      res.status(400).json({error: err.message})
+    } else {
+      return next(err);
+    }
+  }
+});
+
+router.delete('/courses/:id', authenticateUser, async (req, res, next) => {
+  try {
+    const course = await Course.findByPk(req.params.id);
+    if (course === null) {
+      res.status(404).json({message: "This course does not exist"});
+    } else {
+      await course.destroy();
+      res.status(204).end();
+    }
+  } catch(err) {
+    return next(err);
+  }
+})
 
 module.exports = router;
